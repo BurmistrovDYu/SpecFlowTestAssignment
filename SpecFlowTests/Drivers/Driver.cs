c186a87be8814ecdb33f4e64f36d7110876606d2
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 15:38:40 +0300

    Initial commit

---
+++ SpecFlowTests/Drivers/Driver.cs
@@ -0,0 +1,6 @@
+namespace SpecFlowTests.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ SpecFlowTests/Hooks/Hook.cs
@@ -0,0 +1,9 @@
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
