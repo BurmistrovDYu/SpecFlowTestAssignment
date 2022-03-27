887835f5f163cbd4e2f2249c2e1653f4e941bcc8
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 17:31:05 +0300

    Implement GoogleMainPageSteps bindings

---
+++ SpecFlowTests/Steps/GoogleMainPageSteps.cs
@@ -0,0 +1,29 @@
+﻿using SpecFlowTests.Drivers;
+using SpecFlowTests.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests.Steps
+{
+    [Binding]
+    public class GoogleMainPageSteps
+    {
+        private readonly GoogleMainPage _googleMainPage;
+        
+        public GoogleMainPageSteps(Driver driver)
+        {
+            _googleMainPage = new GoogleMainPage(driver.CurrentDriver);
+        }
+        
+        [Given("(.*) page is opened")]
+        public void GivenWebsiteIsOpened(string url)
+        {
+            _googleMainPage.VerifyUrlIsCorrect(url);
+        }
+        
+        [Given("search field is visible")]
+        public void GivenSearchFieldIsVisible()
+        {
+            _googleMainPage.VerifyInputSearchFieldIsVisible();
+        }
+    }
+}
