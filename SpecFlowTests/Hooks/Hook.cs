750330f0d855f0c8f0354e55609f325346801fd8
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 17:32:43 +0300

    Implement BeforeScenario and AfterScenario hooks

--- SpecFlowTests/Hooks/Hook.cs
+++ SpecFlowTests/Hooks/Hook.cs
@@ -1,3 +1,5 @@
+using BoDi;
+using SpecFlowTests.Drivers;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowTests.Hooks
@@ -5,5 +7,18 @@ namespace SpecFlowTests.Hooks
     [Binding]
     public class Hooks
     {
+        [BeforeScenario]
+        public static void GoogleSearchSetup(ObjectContainer container)
+        {
+            var driver = container.Resolve<Driver>();
+            driver.CurrentDriver.Url = "https://www.google.com";
+        }
+
+        [AfterScenario]
+        public static void GoogleSearchTeardown(ObjectContainer container)
+        {
+            var driver = container.Resolve<Driver>();
+            driver.Dispose();
+        }
     }
 }
