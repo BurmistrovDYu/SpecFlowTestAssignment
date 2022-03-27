9d44264bec59722e545a43a74cd6dbce7a750616
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 17:42:44 +0300

    Implement VerifyInputSearchFieldTextIsCorrect, SetInputSearchFieldValue, ClickButtonGoogleSearch methods

--- SpecFlowTests/PageObjects/GoogleMainPage.cs
+++ SpecFlowTests/PageObjects/GoogleMainPage.cs
@@ -11,10 +11,20 @@ namespace SpecFlowTests.PageObjects
         }
 
         private IWebElement inputSearchField => webDriver.FindElement(By.XPath("//input[@role='combobox']"));
+        private IWebElement buttonGoogleSearch => webDriver.FindElement(By.XPath("//input[@name='btnK' and not(ancestor::div[contains(@style, 'display:none')])]"));
 
         #region Actions
 
-        
+        public void SetInputSearchFieldValue(string text)
+        {
+            inputSearchField.Clear();
+            inputSearchField.SendKeys(text);
+        }
+
+        public void ClickButtonGoogleSearch()
+        {
+            buttonGoogleSearch.Click();
+        }
 
         #endregion
 
@@ -24,6 +34,12 @@ namespace SpecFlowTests.PageObjects
         {
             var actual = inputSearchField.Displayed;
             Assert.AreEqual(expected, actual, "Verify search field is visible");
+        }
+
+        public void VerifyInputSearchFieldTextIsCorrect(string expected)
+        {
+            var actual = inputSearchField.GetAttribute("value");
+            Assert.AreEqual(expected, actual, "Verify search field is correct");
         }
 
         #endregion
