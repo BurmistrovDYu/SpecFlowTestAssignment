e596c5c353d609d941f07d92f95c74fd3a5f1d49
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 17:11:22 +0300

    Implement BasePage page object

---
+++ SpecFlowTests/PageObjects/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SpecFlowTests.PageObjects
+{
+    public class BasePage
+    {
+        protected readonly IWebDriver webDriver;
+
+        public BasePage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        #region Actions
+
+        public void OpenUrl(string url)
+        {
+            if (webDriver.Url != url)
+            {
+                webDriver.Url = url;
+            }
+        }
+
+        #endregion
+
+        #region Verification
+
+        public void VerifyUrlIsCorrect(string expected)
+        {
+            var actual = webDriver.Url;
+            Assert.AreEqual(expected, actual, "Verify url is correct");
+        }
+
+        #endregion
+    }
+}
