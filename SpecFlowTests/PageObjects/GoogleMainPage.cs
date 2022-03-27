206aac82e61d2efc2041450082e53b41a4f430a2
Burmistrov, Denys <dburm@softserveinc.com>
2022-03-27 17:18:00 +0300

    Implement GoogleMainPage page object

---
+++ SpecFlowTests/PageObjects/GoogleMainPage.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SpecFlowTests.PageObjects
+{
+    public class GoogleMainPage : BasePage
+    {
+        GoogleMainPage(IWebDriver driver) : base(driver)
+        {
+            
+        }
+
+        private IWebElement inputSearchField => webDriver.FindElement(By.XPath("//input[@role='combobox']"));
+
+        #region Actions
+
+        
+
+        #endregion
+
+        #region Verification
+
+        public void VerifyInputSearchFieldIsVisible(bool expected = true)
+        {
+            var actual = inputSearchField.Displayed;
+            Assert.AreEqual(expected, actual, "Verify search field is visible");
+        }
+
+        #endregion
+    }
+}
